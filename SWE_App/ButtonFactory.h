85796ec6aac0b4cc170c61530a31575b34999499
Bluefirepiro <Bluefirepiro@gmail.com>
2023-08-14 02:08:32 -0400

    Add project files.

---
+++ SWE_App/ButtonFactory.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "wx/wx.h"
+#include "Window.h"
+
+static class ButtonFactory
+
+{
+public:
+	static wxButton* CreateButton(Window* win, wxWindowID id, const wxString str, const wxPoint point, const wxSize size, const wxFont font);
+	static wxButton* CreateClearButton(Window* win, wxWindowID id, const wxString str, const wxPoint point, const wxSize size);
+	static wxButton* CreateEqualsButton(Window* win, wxWindowID id, const wxString str, const wxPoint point, const wxSize size);
+	static wxButton* CreateNegativeButton(Window* win, wxWindowID id, const wxString str, const wxPoint point, const wxSize size, const wxFont font);
+
+    
+};
