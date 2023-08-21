d980ffcf023f02b9650b4e44a25b9155b189e885
Bluefirepiro <Bluefirepiro@gmail.com>
2023-08-21 12:48:21 -0400

    Calculator

--- SWE_App/Window.h
+++ SWE_App/Window.h
@@ -5,46 +5,46 @@
 class Window : public wxFrame
 {
 public:
-	Window();
-	~Window();
+    Window();
+    ~Window();
 
 public:
-	wxTextCtrl* textbox = nullptr;
-	wxButton* button = nullptr;
-	wxButton* clearButton = nullptr;
-	wxButton* equalsButton = nullptr;
+    wxTextCtrl* textbox = nullptr;
+    wxButton* button = nullptr;
+    wxButton* clearButton = nullptr;
+    wxButton* equalsButton = nullptr;
 
-	void OnButtonClicked(wxCommandEvent& evt);
-	void OnButtonClear(wxCommandEvent& evt);
-	void OnButtonNegative(wxCommandEvent& evt);
-	void OnButtonEquals(wxCommandEvent& evt);
-	void OnCloseWindow(wxCloseEvent& evt);
-	wxDECLARE_EVENT_TABLE();
+    void OnButtonClicked(wxCommandEvent& evt);
+    void OnButtonClear(wxCommandEvent& evt);
+    void OnButtonNegative(wxCommandEvent& evt);
+    void OnButtonEquals(wxCommandEvent& evt);
+    void OnCloseWindow(wxCloseEvent& evt);
+    wxDECLARE_EVENT_TABLE();
 
-	enum BUTTONS
-	{
-		ONE,
-		TWO,
-		THREE,
-		FOUR,
-		FIVE,
-		SIX,
-		SEVEN,
-		EIGHT,
-		NINE,
-		ZERO,
-		PLUS,
-		MINUS,
-		MULTIPLY,
-		DIVIDE,
-		MODULUS,
-		EQUALS,
-		RIGHT,
-		LEFT,
-		NEGATIVE,
-		COS,
-		SIN,
-		TAN,
-		CLEAR
-	};
+    enum BUTTONS
+    {
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        ZERO,
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE,
+        MODULUS,
+        EQUALS,
+        RIGHT,
+        LEFT,
+        NEGATIVE,
+        COS,
+        SIN,
+        TAN,
+        CLEAR
+    };
 };
